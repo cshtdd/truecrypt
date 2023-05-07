94ab6938ae466b0399a10f0ef5a6c4e8680c522d
Camilo Sanchez <camilin87@gmail.com>
2023-05-07 11:54:03 -0700

    Build the program for multiple architectures

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Build binaries for multiple architectures
+#
+# Inspiration:
+# https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
+
+BINARY="bin/truecrypt_"
+
+SETTINGS=("darwin_arm64" "darwin_amd64")
+
+for suffix in "${SETTINGS[@]}"
+do
+    suffix_pieces=(${suffix/_/ })
+	GOOS=${suffix_pieces[0]}
+	GOARCH=${suffix_pieces[1]}
+    BINARY_PATH="${BINARY}${suffix}"
+
+    echo "Testing Os: ${GOOS} Arch: ${GOARCH}"
+    env GOOS=$GOOS GOARCH=$GOARCH go clean -testcache || exit 1
+    env GOOS=$GOOS GOARCH=$GOARCH go test ./... || exit 1
+
+    echo "Building for Os: ${GOOS} Arch: ${GOARCH} Output: ${BINARY_PATH}"
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o "${BINARY_PATH}" || exit 1
+done
--- clean.sh.command
+++ clean.sh.command
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname -- "$0")"
-$SCRIPT_DIR/truecrypt -clean
+OS_NAME="$(uname | tr '[:upper:]' '[:lower:]')"
+ARCH_NAME="$(uname -m)"
+$SCRIPT_DIR/bin/truecrypt_${OS_NAME}_${ARCH_NAME} -clean
