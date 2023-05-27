8ddfd5354f294c835c274b16e572abe55cb73253
Camilo Sanchez <camilin87@gmail.com>
2023-05-27 07:40:38 -0700

    Minor refactor for the matching helpers

--- internal/test/helpers/helpers.go
+++ internal/test/helpers/helpers.go
@@ -92,59 +92,81 @@ func CreateSampleNestedStructure(t *testing.T, dir paths.Path) {
 	}
 }
 
-func EnsureSamplePathsMatch(t *testing.T, dirA paths.Path, dirB paths.Path, shouldMatch bool) {
+type MatchType int
+
+const (
+	Err MatchType = iota
+	Mismatch
+	Match
+)
+
+func EnsureSamplePathsMatch(t *testing.T, dirA paths.Path, dirB paths.Path, shouldMatch bool) MatchType {
 	filesB := getSamplePaths(dirB)
 
 	for index, fileA := range getSamplePaths(dirA) {
 		fileB := filesB[index]
-
-		// check existence
-		existsA, err := fileA.Exists()
-		if err != nil {
-			t.Fatalf("Unexpected error checking existence path: %s, err: %s", fileA, err)
-		}
-
-		existsB, err := fileB.Exists()
-		if err != nil {
-			t.Fatalf("Unexpected error checking existence path: %s, err: %s", fileB, err)
-		}
-
-		if existsA != existsB { // mismatch
-			if !shouldMatch { // the test was expecting a mismatch
-				return // return early to pass the test
-			} else {
-				t.Fatalf(
-					"File existence mismatch. a: %s, a_exists: %t, b: %s, b_exists: %t",
-					fileA, existsA, fileB, existsB,
-				)
+		switch m := EnsureFilesMatch(t, fileA, fileB, shouldMatch); {
+		case m == Match:
+			if !shouldMatch { // the test was expected a mismatch
+				break
 			}
-		}
-
-		if existsA && existsB {
-			bytesA, err := fileA.Read()
-			if err != nil {
-				t.Fatalf("Unexpected error reading fileA: %s", fileA)
-			}
-
-			bytesB, err := fileB.Read()
-			if err != nil {
-				t.Fatalf("Unexpected error reading fileB: %s", fileB)
-			}
-
-			if !bytes.Equal(bytesA, bytesB) { // mismatch
-				if !shouldMatch { // the test was expecting a mismatch
-					return // return early to pass the test
-				} else {
-					t.Fatalf(
-						"File contents mismatch. a: %s, a_data: %s, b: %s, b_data: %s",
-						fileA, bytesA, fileB, bytesB,
-					)
-				}
-			}
+		default:
+			return m
 		}
 	}
 
 	if !shouldMatch {
 		t.Fatalf("Directories match. want false, a: %s, b: %s", dirA, dirB)
 	}
+	return Match
+}
+
+func EnsureFilesMatch(t *testing.T, fileA paths.Path, fileB paths.Path, shouldMatch bool) MatchType {
+	existsA, err := fileA.Exists()
+	if err != nil {
+		t.Fatalf("Unexpected error checking existence path: %s, err: %s", fileA, err)
+		return Err
+	}
+
+	existsB, err := fileB.Exists()
+	if err != nil {
+		t.Fatalf("Unexpected error checking existence path: %s, err: %s", fileB, err)
+		return Err
+	}
+
+	if existsA != existsB { // mismatch
+		if shouldMatch { // the test was expecting match
+			t.Fatalf(
+				"File existence mismatch. a: %s, a_exists: %t, b: %s, b_exists: %t",
+				fileA, existsA, fileB, existsB,
+			)
+		}
+		return Mismatch
+	}
+
+	if existsA && existsB {
+		bytesA, err := fileA.Read()
+		if err != nil {
+			t.Fatalf("Unexpected error reading fileA: %s", fileA)
+			return Err
+		}
+
+		bytesB, err := fileB.Read()
+		if err != nil {
+			t.Fatalf("Unexpected error reading fileB: %s", fileB)
+			return Err
+		}
+
+		if !bytes.Equal(bytesA, bytesB) { // mismatch
+			if shouldMatch { // the test was expecting a match
+				t.Fatalf(
+					"File contents mismatch. a: %s, a_data: %s, b: %s, b_data: %s",
+					fileA, bytesA, fileB, bytesB,
+				)
+			}
+			return Mismatch
+		}
+	}
+
+	return Match
 }
